429967c71bcd2dbe4eeb011cb62b5db88a10df46
RenanSaravalli <renansaravalli@gmail.com>
2025-06-05 09:17:26 -0300

    First Commit - Adicionando primeiras aulas e atividades (02b)

---
+++ src/aula02/atividade/Serie.java
@@ -0,0 +1,45 @@
+package aula02.atividade;
+
+public class Serie {
+    String nome;
+    String empresa;
+    String dataLancamento;
+    int duracao;
+    boolean assistida = false;
+    int avaliacao;
+    String[] elenco;
+
+    void assistirSerie() {
+        assistida = true;
+        System.out.println("Parabéns você assistiu a série: " + this.nome);
+    }
+
+    void avaliarSerie(int nota) {
+        if (nota > 10) {
+            this.avaliacao = 10;
+        } else if (nota < 0) {
+            this.avaliacao = 0;
+        } else {
+            this.avaliacao = nota;
+        }
+    }
+
+    void adicionarAtores(String[] listaAtores) {
+        this.elenco = listaAtores;
+    }
+
+    void statusSerie() {
+        System.out.println("-------");
+        System.out.println("Nome:            " + this.nome);
+        System.out.println("Empresa:         " + this.empresa);
+        System.out.println("Data Lançamento: " + this.dataLancamento);
+        System.out.println("Duração:         " + this.duracao + " Minutos");
+        System.out.println("Assistida:       " + this.assistida);
+        System.out.println("Avaliação nota:  " + this.avaliacao);
+        System.out.print("Elenco:          ");
+        for (int i = 0; i < this.elenco.length; i++) {
+            System.out.print(this.elenco[i] + " ");
+        }
+
+    }
+}
---
+++ src/aula02/atividade/TesteSerie.java
@@ -0,0 +1,17 @@
+package aula02.atividade;
+
+public class TesteSerie {
+    public static void main(String[] args) {
+        Serie naruto = new Serie();
+        naruto.nome = "Naruto";
+        naruto.empresa = "Masashi Kishimoto";
+        naruto.dataLancamento = "03-10-2002";
+        naruto.duracao = 1108;
+
+
+        naruto.adicionarAtores(new String[]{"Naruto", "Sasuke", "Sakura", "Kakashi", "Gai", "Rocke lee", "Neji", "Obito"});
+        naruto.assistirSerie();
+        naruto.avaliarSerie(79);
+        naruto.statusSerie();
+    }
+}
