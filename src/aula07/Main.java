779bffe179eb3a8e7be00630adca1de93be21779
RenanSaravalli <renansaravalli@gmail.com>
2025-06-16 23:08:20 -0300

    Criando a classe lutador da aula prática 07

---
+++ src/aula07/Main.java
@@ -0,0 +1,12 @@
+package aula07;
+
+public class Main {
+    public static void main(String[] args) {
+        Lutador[] l = new Lutador[6];
+
+        l[0] = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
+        l[0].status();
+        l[1] = new Lutador("Putsript", "Brasil", 29, 1.68f, 57.8f,14,2,3);
+        l[1].status();
+    }
+}
