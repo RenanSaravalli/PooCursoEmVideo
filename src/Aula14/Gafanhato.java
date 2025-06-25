b759ea51c2e12c6596beea18773dce8a9d9afa71
RenanSaravalli <renansaravalli@gmail.com>
2025-06-24 21:32:17 -0300

    Finalizando a primeira parte do últipo exercício do curso de POO no curso em vídeo

--- src/Aula14/Gafanhato.java
+++ src/Aula14/Gafanhato.java
@@ -4,12 +4,49 @@ public class Gafanhato extends Pessoa{
     private String login;
     private int totAssistido;
 
+    public Gafanhato() {
+    }
+
+    public Gafanhato(String nome, int idade, String sexo, String login) {
+        super(nome, idade, sexo);
+        this.login = login;
+        this.totAssistido = 0;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getTotAssistido() {
+        return totAssistido;
+    }
+
+    public void setTotAssistido(int totAssistido) {
+        this.totAssistido = totAssistido;
+    }
+
     public void viuMaisUm() {
         System.out.println("Assistindo mais um vídeo");
     }
 
     @Override
     protected void ganharExperiencia() {
+        setExperiencia(getExperiencia()+1);
+    }
 
+    @Override
+    public String toString() {
+        return "Gafanhato{" +
+                ", nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", sexo='" + sexo + '\'' +
+                ", experiencia=" + experiencia +
+                " login='" + login + '\'' +
+                ", totAssistido=" + totAssistido +
+                '}';
     }
 }
--- src/Aula14/Main.java
+++ src/Aula14/Main.java
@@ -2,6 +2,27 @@ package Aula14;
 
 public class Main {
     public static void main(String[] args) {
+        Video[] v = new Video[3];
+        v[0] = new Video("Musica Caipira");
+        v[1] = new Video("ROck LEe VS Gaara ao som de Linkin Park");
+        v[2] = new Video("Serie minecraft");
 
+        Gafanhato[] g = new Gafanhato[2];
+
+        g[0] = new Gafanhato("Renan", 19, "M", "re");
+        g[1] = new Gafanhato("Ana", 19, "F", "Aninha");
+
+        System.out.println("---- Vídeos ----");
+        System.out.println(v[0]);
+        System.out.println(v[1]);
+        System.out.println(v[2]);
+
+        System.out.println(" ");
+
+        System.out.println("---- Gafanhotos ----");
+        System.out.println(g[0]);
+        System.out.println(g[1]);
     }
+
+
 }
